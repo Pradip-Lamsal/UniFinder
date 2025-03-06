c906ee57425d66ce61bfb9544573c53ecb0be94a
Pradip Lamsal <pradiplamsal80@gmail.com>
2025-03-06 08:49:39 +0545

    done

---
+++ uni_finder copy 2/lib/features/auth/domain/entity/course.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class Course extends Equatable {
+  final String id;
+  final String courseType;
+  final String courseCategory;
+  final String? instructor;
+
+  const Course({
+    required this.id,
+    required this.courseType,
+    required this.courseCategory,
+    this.instructor,
+  });
+
+  @override
+  List<Object?> get props => [id, courseType, courseCategory, instructor];
+}
