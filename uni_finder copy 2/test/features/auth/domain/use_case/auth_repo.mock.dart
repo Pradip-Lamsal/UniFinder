bac78978ac9f5289f6b0b3161d7d0249436017c0
Pradip Lamsal <pradiplamsal80@gmail.com>
2025-03-06 09:01:00 +0545

    repo

---
+++ uni_finder copy 2/test/features/auth/domain/use_case/auth_repo.mock.dart
@@ -0,0 +1,4 @@
+import 'package:bus_application/features/auth/domain/repository/auth_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class AuthRepoMock extends Mock implements IAuthRepository {}
