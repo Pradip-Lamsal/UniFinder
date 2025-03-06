75c119a9ecffa493accb2154e16b3564d92e7f93
Pradip Lamsal <pradiplamsal80@gmail.com>
2025-03-06 14:45:14 +0545

    done

--- uni_finder copy 2/lib/app/app.dart
+++ uni_finder copy 2/lib/app/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../core/hive/hive_setup.dart';
 import '../features/auth/presentation/view_model/login/login_bloc.dart';
-import '../hive/hive_setup.dart';
 import 'di/di.dart';
 import 'routes.dart';
 
