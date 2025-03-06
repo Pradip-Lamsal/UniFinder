2c643d00302e1eb9ff271ac768275a06b9d42791
Pradip Lamsal <pradiplamsal80@gmail.com>
2025-03-06 08:35:25 +0545

    ok

--- uni_finder/lib/app/app.dart
+++ uni_finder/lib/app/app.dart
@@ -1,7 +1,7 @@
+import 'package:bus_application/features/splash/presentation/view/onboardingscreen_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../features/auth/presentation/view/login_view.dart';
 import '../features/auth/presentation/view_model/login/login_bloc.dart';
 import 'di/di.dart';
 
@@ -10,12 +10,19 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Customer Management',
-      home: BlocProvider.value(
-        value: getIt<LoginBloc>(),
-        child: LoginView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => getIt<LoginBloc>(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Customer Management',
+        home: BlocProvider.value(
+          value: getIt<LoginBloc>(),
+          child: const OnboardingScreenView(),
+        ),
       ),
     );
   }
