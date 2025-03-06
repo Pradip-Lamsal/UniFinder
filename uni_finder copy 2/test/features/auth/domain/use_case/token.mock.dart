9b66313bad5053229ac0ff147019bce390da9997
Pradip Lamsal <pradiplamsal80@gmail.com>
2025-03-06 09:01:53 +0545

    test

---
+++ uni_finder copy 2/test/features/auth/domain/use_case/token.mock.dart
@@ -0,0 +1,4 @@
+import 'package:bus_application/app/shared_prefs/token_shared_prefs.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockTokenSharedPrefs extends Mock implements TokenSharedPrefs {}
