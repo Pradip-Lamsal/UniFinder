1bf5a8184be1e3e2e8f9186a3613f500dc4d9995
Pradip Lamsal <pradiplamsal80@gmail.com>
2025-03-06 08:53:29 +0545

    done

---
+++ uni_finder copy 2/lib/features/home/data/models/course_model.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'course_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CourseModelAdapter extends TypeAdapter<CourseModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  CourseModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return CourseModel(
+      id: fields[0] as String,
+      courseType: fields[1] as String,
+      courseCategory: fields[2] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, CourseModel obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.courseType)
+      ..writeByte(2)
+      ..write(obj.courseCategory);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CourseModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
