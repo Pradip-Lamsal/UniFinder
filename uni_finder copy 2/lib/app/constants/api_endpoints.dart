c7fe7f90dd9c8e70754ff149c166b27cbe5360e6
Pradip Lamsal <pradiplamsal80@gmail.com>
2025-03-06 14:45:20 +0545

    done

--- uni_finder copy 2/lib/app/constants/api_endpoints.dart
+++ uni_finder copy 2/lib/app/constants/api_endpoints.dart
@@ -4,8 +4,9 @@ class ApiEndpoints {
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
   //static const String baseUrl = "http://10.0.2.2:5001/api/";
-  //static const String baseUrl = "http://172.26.19.88:5001/api/";
-  static const String baseUrl = "http://192.168.172.3:5001/api/";
+  static const String baseUrl = "http://172.26.0.51:5001/api/";
+  //static const String baseUrl = "http://192.168.172.3:5001/api/";
+  // 172.26.0.51
 
   // For iPhone
   //static const String baseUrl = "http://localhost:3000/api/v1/";
@@ -17,8 +18,8 @@ class ApiEndpoints {
   static const String consultancies = "consultancies";
 
   //static const String imageUrl = "http://10.0.2.2:5001/uploads/";
-  static const String imageUrl = "http://192.168.172.3:5001/uploads/";
-  //static const String imageUrl = "http://172.26.19.88:5001/uploads/";
+  //static const String imageUrl = "http://192.168.172.3:5001/uploads/";
+  static const String imageUrl = "http://172.26.0.51:5001/uploads/";
   //192.168.101.8
   static const String uploadImage = "auth/uploadImage";
 }
