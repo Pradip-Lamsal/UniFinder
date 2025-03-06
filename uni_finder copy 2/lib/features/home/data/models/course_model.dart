e23f28b183ddbf517f0545999fd49ed06a68c27e
Pradip Lamsal <pradiplamsal80@gmail.com>
2025-03-06 08:53:15 +0545

    done

---
+++ uni_finder copy 2/lib/features/home/data/models/course_model.dart
@@ -0,0 +1,48 @@
+import 'package:hive/hive.dart';
+
+import '../../../auth/domain/entity/course.dart';
+
+part 'course_model.g.dart';
+
+@HiveType(typeId: 1)
+class CourseModel extends HiveObject {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String courseType;
+
+  @HiveField(2)
+  final String courseCategory;
+
+  CourseModel({
+    required this.id,
+    required this.courseType,
+    required this.courseCategory,
+  });
+
+  factory CourseModel.fromEntity(Course course) {
+    return CourseModel(
+      id: course.id,
+      courseType: course.courseType,
+      courseCategory: course.courseCategory,
+    );
+  }
+
+  // Implement fromJson to deserialize JSON data into CourseModel
+  static CourseModel fromJson(Map<String, dynamic> json) {
+    return CourseModel(
+      id: json['id'] as String,
+      courseType: json['courseType'] as String,
+      courseCategory: json['courseCategory'] as String,
+    );
+  }
+
+  Course toEntity() {
+    return Course(
+      id: id,
+      courseType: courseType,
+      courseCategory: courseCategory,
+    );
+  }
+}
